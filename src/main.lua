8a447990ee711247c821187caf00e702ae5ee9a7
Walter Mays <sircular17@gmail.com>
2018-04-21 10:10:57 -0500

    Set up basic maps and some assets.

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,21 @@
-function love.draw()
+local gamera = require('lib/gamera')
+local bump   = require('lib/bump')
 
+love.graphics.setDefaultFilter("nearest", "nearest")
+
+local Map = require('map')
+-- local Guard = require('entities/guard')
+
+-- local g = Guard:new(10, 10)
+local world = bump.newWorld()
+local m = Map:new("maps/test.lua", world)
+local cam = gamera.new(0, 0, 20000, 20000)
+cam:setScale(2)
+cam:setPosition(0, 0)
+
+
+function love.draw()
+  cam:draw(function(l, t, w, h)
+    m:draw()
+  end)
 end
---
+++ src/map.lua
@@ -0,0 +1,47 @@
+local Class = require('lib/middleclass')
+
+local Map = Class('Map')
+
+-- includes a bump world that requires entities
+function Map:initialize(path, world)
+  local data = dofile(path)
+
+  -- in tiles
+  self.width    = data.width
+  self.height   = data.height
+  self.tileSize = data.tileSize -- in pixels
+  self.tiles    = {}
+
+
+  local tileImg = love.graphics.newImage(data.tileSet)
+  -- construct quads
+  local quads = {}
+  local ts = self.tileSize
+  for y = 0, math.floor(tileImg:getHeight()/ts)-1 do
+    for x = 0, math.floor(tileImg:getWidth()/ts)-1 do
+      quads[#quads+1] = love.graphics.newQuad(x*ts, y*ts, ts, ts, tileImg:getDimensions())
+    end
+  end
+
+  self.tileBatch = love.graphics.newSpriteBatch(tileImg, 2048)
+
+  for i = 1, #data.tiles do
+    local x = (i-1)%self.width
+    local y = ((i-1)-x)/self.width
+    if data.tiles[i] and data.tiles[i] > 0 then
+      local t = {tileId = data.tiles[i], x = x, y = y}
+      self.tiles[#self.tiles+1] = t
+      world:add(t, x*ts, y*ts, ts, ts)
+    end
+    if quads[data.tiles[i]+1] then
+      self.tileBatch:add(quads[data.tiles[i]+1], x*ts, y*ts)
+    end
+  end
+
+end
+
+function Map:draw()
+  love.graphics.draw(self.tileBatch)
+end
+
+return Map
