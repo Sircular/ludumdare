117764188f1edeca8d78e2b2a16fe8aa2b33f6a5
Walter Mays <sircular17@gmail.com>
2018-04-22 18:12:56 -0500

    Set up configuration a bit more.

--- conf.lua
+++ conf.lua
@@ -1,3 +1,7 @@
 function love.conf(t)
-  t.window.title = "Puzzle Prison"
+  t.window.title  = "Puzzle Prison"
+  t.window.width  = 1024
+  t.window.height = 768
+
+  t.modules.joystick = false
 end
